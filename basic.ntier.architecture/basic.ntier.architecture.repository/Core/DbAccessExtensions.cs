45267f17a45c6c6d67f5a8f1dbf244ca470544c3
john-lay <john_lay@hotmail.co.uk>
2015-07-14 19:50:45 +0100

    Added missing files

---
+++ basic.ntier.architecture/basic.ntier.architecture.repository/Core/DbAccessExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace basic.ntier.architecture.repository.Core
+{
+    public static class DbAccessExtensions
+    {
+        public static T To<T>(this DbDataReader r, string fieldName, T defaultValue = default(T))
+        {
+            object value = r[fieldName];
+            return Converter.ConvertTo<T>(value, defaultValue);
+        }
+    }
+}
---
+++ basic.ntier.architecture/basic.ntier.architecture.repository/Customer/CustomerRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using basic.ntier.architecture.entity.Customer;
+using basic.ntier.architecture.repository.Core;
+
+namespace basic.ntier.architecture.repository.Customer
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public CustomerEntity GetCustomer(long customerId)
+        {
+            var sqlAccess = new SqlServerAccess("stp_getCustomer");
+            sqlAccess.AddParameter("@CustomerId", customerId);
+
+            return sqlAccess.ExecuteReaderFirstRowIfExists(r => new CustomerEntity
+            {
+                CustomerId = r.To<long>("Id"),
+                FirstName = r.To<string>("FirstName"),
+                LastName = r.To<string>("LastName")
+            });
+        }
+    }
+}

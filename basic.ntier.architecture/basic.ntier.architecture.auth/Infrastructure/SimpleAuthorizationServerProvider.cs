b426aacd0818eb27a1df427328211c47666d32d4
john-lay <john_lay@hotmail.co.uk>
2018-02-22 10:33:33 +0000

    Added OAuth2 authentication with JWT

---
+++ basic.ntier.architecture/basic.ntier.architecture.auth/Infrastructure/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,44 @@
+﻿namespace basic.ntier.architecture.auth.Infrastructure
+{
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using basic.ntier.architecture.auth.Models;
+    using basic.ntier.architecture.auth.Stores;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.Owin.Security.OAuth;
+
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        private readonly UserManager<IdentityUser, string> userManager;
+
+        public SimpleAuthorizationServerProvider()
+        {
+            var userStore = new UserStore<IdentityUser>();
+            userManager = new UserManager<IdentityUser, string>(userStore);
+        }
+
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            IdentityUser user = userManager.Find(context.UserName, context.Password);
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+        }
+    }
+}
--- basic.ntier.architecture/basic.ntier.architecture.auth/Startup.cs
+++ basic.ntier.architecture/basic.ntier.architecture.auth/Startup.cs
@@ -3,7 +3,11 @@
 [assembly: OwinStartup(typeof(basic.ntier.architecture.auth.Startup))]
 namespace basic.ntier.architecture.auth
 {
+    using System;
     using System.Web.Http;
+    using basic.ntier.architecture.auth.Infrastructure;
+    using Microsoft.Owin.Cors;
+    using Microsoft.Owin.Security.OAuth;
     using Owin;
 
     public class Startup
@@ -13,6 +17,23 @@ namespace basic.ntier.architecture.auth
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+            app.UseCors(CorsOptions.AllowAll);
+            ConfigureOAuth(app);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 }
